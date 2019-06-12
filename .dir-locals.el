959fd92d9f237d041f8991b9cac959fd5cff61da
Anton Chistyakov <nuttzipper@gmail.com>
2019-06-12 11:54:19 +0300

    cleanup

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;; https://emacs.stackexchange.com/a/24188
-((js-mode
-  ;; TabsAreEvil
-  (indent-tabs-mode . nil)
-  ;; match airbnb style (following agoric / erights)
-  (js2-basic-offset . 2)
-  (js2-indent-switch-body . true)
-  ))
